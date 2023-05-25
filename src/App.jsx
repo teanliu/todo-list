d2f3a01d0e7899831a345aa5594f9d4b9e73af24
teanliu <teanliuu@gmail.com>
2023-05-26 00:01:37 +0800

    todolist init

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.scss';
+import { TodoPage, LoginPage, SignUpPage, HomePage } from './pages';
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+function App() {
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="*" element={<HomePage />} />
+          <Route path="login" element={<LoginPage />} />
+          <Route path="signup" element={<SignUpPage />} />
+          <Route path="todo" element={<TodoPage />} />
+        </Routes>
+      </BrowserRouter>
+      <TodoPage />
+    </div>
+  );
+}
+
+export default App;
