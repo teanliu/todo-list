6104aff87578800661335635b9929f3bd95f7bbe
teanliu <teanliuu@gmail.com>
2023-05-31 14:08:20 +0800

    feat: path problem

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 //   }),
 // );
 
-app.get('/*', function (req, res) {
+app.get('/login', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
