0c20cc0d2d4907724242fc2c2143761b0acebf14
teanliu <teanliuu@gmail.com>
2023-05-31 13:33:48 +0800

    feat: Heroku update

--- server.js
+++ server.js
@@ -5,10 +5,13 @@ const middlewares = jsonServer.defaults({
   static: './build',
 });
 const PORT = process.env.PORT || 8000;
-
 server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    '/api/*': '/$1',
+  }),
+);
 server.use(router);
-
 server.listen(PORT, () => {
-  console.log('Server is running on Heroku...');
+  console.log('Server is running');
 });
