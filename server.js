40dd6d0d858044964f95811ae0f874c72236085d
teanliu <teanliuu@gmail.com>
2023-05-31 14:04:37 +0800

    feat: Heroku server.js

--- server.js
+++ server.js
@@ -1,16 +1,24 @@
 const jsonServer = require('json-server');
-const app = jsonServer.create();
 const path = require('path');
 const express = require('express');
+const app = jsonServer.create();
+const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults();
-const router = jsonServer.router('db.json');
-const port = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8000;
+
 
 app.use('/db', middlewares, router);
 app.use(express.static(path.join(__dirname, 'build')));
+// app.use(
+//   jsonServer.rewriter({
+//     '/api/*': '/$1',
+//   }),
+// );
 
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(port);
+app.listen(PORT, () => {
+  console.log('Server is running on Heroku...');
+});
