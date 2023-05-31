6f239a65ab61bf1c30c6e2f42c3c33f449372ee3
teanliu <teanliuu@gmail.com>
2023-05-31 14:50:53 +0800

    feat: update page error

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
 const jsonServer = require('json-server');
-const server = jsonServer.create();
+const app = jsonServer.create();
+const path = require('path');
+const express = require('express');
+const middlewares = jsonServer.defaults();
 const router = jsonServer.router('./db.json');
-const middlewares = jsonServer.defaults({
-  static: './build',
-});
 const PORT = process.env.PORT || 8000;
-server.use(middlewares);
-server.use(router);
-server.listen(PORT, () => {
+
+app.use(middlewares);
+app.use(router);
+// app.use('/db', middlewares, router);
+app.use(express.static(path.join(__dirname, 'build')));
+
+if (process.env.NODE_ENV === "production" || process.env.NODE_ENV === "staging") {
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  });
+}
+
+
+app.listen(PORT, () => {
   console.log('Server is running on Heroku');
 });
