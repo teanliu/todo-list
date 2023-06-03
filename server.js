3f4706a86aca3713904040337f73a2298908af00
teanliu <teanliuu@gmail.com>
2023-06-04 07:10:13 +0800

    feat: update server from express to API server

--- server.js
+++ server.js
@@ -22,3 +22,6 @@ server.get('*', (req, res) => {
 server.listen(PORT, () => {
   console.log(`Server is running on port: ${PORT}`);
 });
+
+// put in package.json/ scripts
+// "heroku-postbuild": "npm run build"
--- src/api/todos.js
+++ src/api/todos.js
@@ -1,10 +1,28 @@
 import axios from 'axios';
+const baseUrl = 'https://todo-list.alphacamp.io/api';
 // const baseUrl = 'http://localhost:8000/api';
-const baseUrl = 'https://todo-list-reactapplication.herokuapp.com/api';
+// const baseUrl = 'https://todo-list-reactapplication.herokuapp.com/api';
+
+const axiosInstance = axios.create({
+  baseURL: baseUrl,
+})
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (e) => {
+    console.error(e);
+  },
+);
 
 export const getTodos = async () => {
   try {
-    const res = await axios.get(`${baseUrl}/todos`);
+    const res = await axiosInstance.get(`${baseUrl}/todos`);
     return res.data;
   } catch (e) {
     console.error('[get todos failed]: ', e);
@@ -14,7 +32,7 @@ export const getTodos = async () => {
 export const createTodo = async (payload) => {
   const { title, isDone } = payload;
   try {
-    const res = await axios.post(`${baseUrl}/todos`, {
+    const res = await axiosInstance.post(`${baseUrl}/todos`, {
       title,
       isDone,
     });
@@ -28,7 +46,7 @@ export const createTodo = async (payload) => {
 export const patchTodo = async (payload) => {
   const { id, title, isDone } = payload;
   try {
-    const res = await axios.patch(`${baseUrl}/todos/${id}`, {
+    const res = await axiosInstance.patch(`${baseUrl}/todos/${id}`, {
       title,
       isDone,
     });
@@ -40,7 +58,7 @@ export const patchTodo = async (payload) => {
 
 export const deleteTodo = async (id) => {
   try {
-    const data = await axios.delete(`${baseUrl}/todos/${id}`);
+    const data = await axiosInstance.delete(`${baseUrl}/todos/${id}`);
     return data.data;
   } catch (e) {
     console.error("[delete todos failed]: ", e);
